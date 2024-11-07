1b0841a888a16638cd9ce63a3c604583c538e37f
Yuta Ishihara <s23C1013PN@s.chibakoudai.jp>
2024-11-07 11:54:50 +0900

    ochinpo

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 ng () {
 	echo ${1}行目がくさいよ
@@ -6,8 +6,16 @@ ng () {
 }
 
 res=0
-a=金子
-[ "$a" = 金玉 ] || ng "$LINENO"
-[ "$a" = 金子 ] || ng "$LINENO"
 
+### 正常位 ###
+out=$(seq おっぱい | ./plus)
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+### 間違位 ###  
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit $res
